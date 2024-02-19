75d786a12cbf06ad504a9f8cb1194bc0920edd61
anjaniprayaga <anjaniprayaga@DESKTOP-8TUAUCB>
2024-02-19 20:37:00 +0530

    Add project files.

---
+++ ResumebuilderAPI/Controllers/ResumeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace ResumebuilderAPI.Controllers
+{
+    [Route("[controller]")]
+    //[Route("api/Resume")]
+    public class ResumeController : ControllerBase
+    {
+        [HttpPost]
+        public HttpResponseMessage CreateResume([FromBody] JObject payload)
+        {
+            
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetResume()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+
+    }
+}
